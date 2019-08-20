a958f925478427db5046ccfb6c94a180f6651097
ogoes <otavio@ogoes.dev>
2019-08-20 11:41:21 -0300

    Estrutura Base

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semicolon: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+};
