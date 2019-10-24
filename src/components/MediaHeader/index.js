eb3a6639ece439105bc3dcae654d11df959484b6
Jorge Rossi <jorgefzrossi@gmail.com>
2019-10-24 10:51:05 -0300

    Método pop header

--- src/components/MediaHeader/index.js
+++ src/components/MediaHeader/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 
 import {Container, Header, CloseIcon, Center, Next} from './styles';
@@ -6,9 +6,6 @@ import {Container, Header, CloseIcon, Center, Next} from './styles';
 export default function MediaHeader(props) {
   const {navigation} = props;
 
-  useEffect(() => {
-    console.log(navigation.goBack);
-  }, [navigation.goBack]);
   return (
     <Container>
       <Header>
