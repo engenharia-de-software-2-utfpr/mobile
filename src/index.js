81e6ff0810b543f1cc8fb5795d0c05c71636af98
Jorge Rossi <jorgefzrossi@gmail.com>
2019-10-24 08:56:52 -0300

    Adicionando reactotron

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React from 'react';
 // import { Container } from './styles';
-import AppContainer from './routes'
+import AppContainer from './routes';
+
+if (__DEV__) {
+  import('./config/ReactotronConfig').then(() =>
+    console.log('Reactotron Configured'),
+  );
+}
 
 export default function App() {
-  return (
-    <AppContainer />
-  )
+  return <AppContainer />;
 }
