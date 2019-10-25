cce67aa95f190402d3bf2e6aba5c4d78226632f2
Jorge Rossi <jorgefzrossi@gmail.com>
2019-10-25 20:53:31 -0300

    Estrutura tela detalhes

--- src/pages/Home/Occurrences/New/Details/index.js
+++ src/pages/Home/Occurrences/New/Details/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ListI} from 'react-native';
 
-import {Container, DetailsTitle, DetailsInput, CategoryTitle} from './styles';
+import {
+  Container,
+  DetailsTitle,
+  DetailsInput,
+  CategoryTitle,
+  CategoryList,
+  CriticityLevelTitle,
+  CategoryContainer,
+  CategoryChip,
+} from './styles';
 
 export default function Details() {
   return (
@@ -9,8 +18,36 @@ export default function Details() {
       <DetailsTitle>Aqui você pode descrever a ocorrência</DetailsTitle>
       <DetailsInput />
       <CategoryTitle>
-        Em qual categoria sua ocorrência mais se encaixa?
+        Em qual categoria sua ocorrência se encaixa?
       </CategoryTitle>
+
+      <CategoryContainer>
+        <CategoryChip icon="information" onPress={() => console.log('Pressed')}>
+          Example Chip
+        </CategoryChip>
+        <CategoryChip icon="information" onPress={() => console.log('Pressed')}>
+          Example Chip
+        </CategoryChip>
+        <CategoryChip icon="information" onPress={() => console.log('Pressed')}>
+          Example Chip
+        </CategoryChip>
+        <CategoryChip icon="information" onPress={() => console.log('Pressed')}>
+          Example Chip
+        </CategoryChip>
+        <CategoryChip icon="information" onPress={() => console.log('Pressed')}>
+          Example Chip
+        </CategoryChip>
+        <CategoryChip icon="information" onPress={() => console.log('Pressed')}>
+          Example Chip
+        </CategoryChip>
+        <CategoryChip icon="information" onPress={() => console.log('Pressed')}>
+          Example Chip
+        </CategoryChip>
+      </CategoryContainer>
+
+      {/* <CriticityLevelTitle>
+        Se possível, informe o nível de criticidade
+      </CriticityLevelTitle> */}
     </Container>
   );
 }
--- src/pages/Home/Occurrences/New/Details/styles.js
+++ src/pages/Home/Occurrences/New/Details/styles.js
@@ -1,17 +1,18 @@
 import styled from 'styled-components/native';
-import {TextInput} from 'react-native';
+import {TextInput, FlatList} from 'react-native';
+import {Chip} from 'react-native-paper';
 
 export const Container = styled.View`
   flex: 1;
   flex-direction: column;
-  padding: 24px 12px;
+  padding: 0% 8%;
 `;
 
 export const DetailsTitle = styled.Text`
   font-weight: bold;
   font-size: 14px;
   text-align: center;
-  padding-bottom: 5%;
+  padding: 8%;
 `;
 
 export const DetailsInput = styled(TextInput).attrs({
@@ -20,7 +21,7 @@ export const DetailsInput = styled(TextInput).attrs({
   numberOfLines: 8,
   maxLength: 500,
 })`
-  background-color: lightgray;
+  background-color: #eeeeee;
   border-radius: 5;
 `;
 
@@ -28,6 +29,22 @@ export const CategoryTitle = styled.Text`
   font-weight: bold;
   font-size: 14px;
   text-align: center;
-  padding-bottom: 5%;
-  padding-top: 5%;
+  padding: 8%;
+`;
+
+export const CategoryContainer = styled.View`
+  max-height: 10%;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const CategoryChip = styled(Chip)`
+  margin: 4px;
+`;
+
+export const CriticityLevelTitle = styled.Text`
+  font-weight: bold;
+  font-size: 14px;
+  text-align: center;
+  padding: 8%;
 `;
