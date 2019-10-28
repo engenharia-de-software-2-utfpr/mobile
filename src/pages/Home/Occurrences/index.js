7620dc0e45e03f51ec0289308173ef9f6fef4611
Jorge Rossi <jorgefzrossi@gmail.com>
2019-10-28 16:32:24 -0300

    Tela de carregamento e posição atual no mapa

--- src/pages/Home/Occurrences/index.js
+++ src/pages/Home/Occurrences/index.js
@@ -1,17 +1,117 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, ButtonContainer, ButtonText, Container, Map} from './styles';
+import {View} from 'react-native';
+import {requestGeoPermission} from '../../../utils/permissions';
+import Geolocation from '@react-native-community/geolocation';
+import {Circle} from 'react-native-maps';
 
 export default function Occurrence({navigation}) {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  useEffect(() => {
+    requestGeoPermission();
+
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log('hahahah', position);
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      null,
+      {enableHighAccuracy: true},
+    );
+  }, []);
+
   return (
     <Container>
       <Map
-        initialRegion={{
-          latitude: -24.046329,
-          longitude: -52.37802,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+        region={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0,
+          longitudeDelta: 0.003,
         }}
-      />
+        customMapStyle={[
+          {
+            featureType: 'administrative.land_parcel',
+            elementType: 'labels',
+            stylers: [
+              {
+                visibility: 'off',
+              },
+              ,
+            ],
+          },
+          {
+            featureType: 'landscape',
+            stylers: [
+              {
+                visibility: 'simplified',
+              },
+            ],
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text',
+            stylers: [
+              {
+                visibility: 'off',
+              },
+            ],
+          },
+          {
+            featureType: 'poi.business',
+            stylers: [
+              {
+                visibility: 'off',
+              },
+            ],
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.icon',
+            stylers: [
+              {
+                visibility: 'off',
+              },
+            ],
+          },
+          {
+            featureType: 'road.local',
+            elementType: 'labels',
+            stylers: [
+              {
+                visibility: 'off',
+              },
+            ],
+          },
+          {
+            featureType: 'transit',
+            stylers: [
+              {
+                visibility: 'off',
+              },
+            ],
+          },
+          {
+            featureType: 'water',
+            stylers: [
+              {
+                weight: 8,
+              },
+            ],
+          },
+        ]}
+        // onRegionChange={setRegion}
+      >
+        <Circle
+          center={{latitude, longitude}}
+          radius={5}
+          fillColor={'#4db6ac'}
+          strokeColor={'#4db6ac'}
+        />
+      </Map>
 
       <ButtonContainer>
         <Button
